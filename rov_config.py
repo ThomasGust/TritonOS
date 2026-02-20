9860b1a11a413b6a6eadc2a70972805ef9d7ab77
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2026-02-20 14:56:25 -0800

    mmc

--- rov_config.py
+++ rov_config.py
@@ -452,7 +452,7 @@ MAG_FUSION_ENABLE = True
 # Note: Setting MAG_FUSION_PREFER_AK = 0.0 is *almost* equivalent to "mmc"
 # when MMC is present, but MAG_OUTPUT_MODE is clearer and sets `mag_source`
 # appropriately.
-MAG_OUTPUT_MODE = "fused"
+MAG_OUTPUT_MODE = "mmc"
 
 # Relative preference (higher => more weight). Default: trust MMC slightly more.
 MAG_FUSION_PREFER_MMC = 1.6
